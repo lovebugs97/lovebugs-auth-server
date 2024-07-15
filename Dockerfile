38db8fe5a5ee7cbff06bc4246fb489712d9dac22
rivercity310 <h970126@gmail.com>
2024-07-16 02:08:18 +0900

    feat: Dockerfile 작성

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM gradle:8.8-jdk17-jammy as builder
+
+WORKDIR /app
+COPY ./ ./
+RUN gradle clean bootjar
+
+FROM openjdk:17.0-slim
+
+WORKDIR /app
+COPY --from=builder /app/build/libs/*.jar app.jar
+USER nobody
+ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/com/lovebugs/auth/TestClient.java
+++ src/main/java/com/lovebugs/auth/TestClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient("config-server")
+@FeignClient("config-service")
 public interface TestClient {
     @GetMapping("/api/v1/test")
     String getTest();
